e61977c189450933552e7ff87c0c4bb8d82adc0b
Heart-travel <zcgacbc@gmail.com>
2014-10-25 14:51:59 +0800

    9 t0 9 multiply table

---
+++ forfor99/ForFor99.java
@@ -0,0 +1,14 @@
+class ForFor99
+{
+	public static void main(String [] args)
+	{
+		for (int x = 1; x <= 9; x++)
+		{
+			for (int y = 1; y <= x; y++)
+			{
+				System.out.print(x+"*"+y+"="+x*y+"\t");
+			}
+			System.out.println();
+		}
+	}
+}
